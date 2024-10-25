bdd9410a8f80c475293655c62094fffc36141077
Wide97 <mwidesott@gmail.com>
2024-10-25 09:59:32 +0200

    start

---
+++ src/main/java/marcowidesott/BackM2PJ1/BackM2Pj1Application.java
@@ -0,0 +1,13 @@
+package marcowidesott.BackM2PJ1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackM2Pj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackM2Pj1Application.class, args);
+	}
+
+}
---
+++ src/test/java/marcowidesott/BackM2PJ1/BackM2Pj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package marcowidesott.BackM2PJ1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackM2Pj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
