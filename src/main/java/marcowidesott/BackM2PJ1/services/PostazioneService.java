323cd34d333a197052abc8a94ca5d25fefbc5b84
Wide97 <mwidesott@gmail.com>
2024-10-25 14:47:11 +0200

    PostazioneService & PrenotazioneService

--- src/main/java/marcowidesott/BackM2PJ1/services/PostazioneService.java
+++ src/main/java/marcowidesott/BackM2PJ1/services/PostazioneService.java
@@ -1,4 +1,36 @@
 package marcowidesott.BackM2PJ1.services;
 
+import marcowidesott.BackM2PJ1.entities.Postazione;
+import marcowidesott.BackM2PJ1.repositories.PostazioneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PostazioneService {
+
+    @Autowired
+    private PostazioneRepository postazioneRepository;
+
+    public List<Postazione> getAllPostazioni() {
+        return postazioneRepository.findAll();
+    }
+
+    public Optional<Postazione> getPostazioneById(Long id) {
+        return postazioneRepository.findById(id);
+    }
+
+    public Postazione createOrUpdatePostazione(Postazione postazione) {
+        return postazioneRepository.save(postazione);
+    }
+
+    public void deletePostazione(Long id) {
+        postazioneRepository.deleteById(id);
+    }
+
+    public List<Postazione> findByTipoAndCitta(Postazione.Tipo tipo, String citta) {
+        return postazioneRepository.findByTipoAndCittaEdificio(tipo, citta);
+    }
 }
--- src/main/java/marcowidesott/BackM2PJ1/services/PrenotazioneService.java
+++ src/main/java/marcowidesott/BackM2PJ1/services/PrenotazioneService.java
@@ -1,4 +1,43 @@
 package marcowidesott.BackM2PJ1.services;
 
+import marcowidesott.BackM2PJ1.entities.Postazione;
+import marcowidesott.BackM2PJ1.entities.Prenotazione;
+import marcowidesott.BackM2PJ1.entities.Utente;
+import marcowidesott.BackM2PJ1.repositories.PrenotazioneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PrenotazioneService {
+
+    @Autowired
+    private PrenotazioneRepository prenotazioneRepository;
+
+    public List<Prenotazione> getAllPrenotazioni() {
+        return prenotazioneRepository.findAll();
+    }
+
+    public Optional<Prenotazione> getPrenotazioneById(Long id) {
+        return prenotazioneRepository.findById(id);
+    }
+
+    public Prenotazione createOrUpdatePrenotazione(Prenotazione prenotazione) {
+        return prenotazioneRepository.save(prenotazione);
+    }
+
+    public void deletePrenotazione(Long id) {
+        prenotazioneRepository.deleteById(id);
+    }
+
+    public List<Prenotazione> findByPostazioneAndData(Postazione postazione, LocalDate dataPrenotazione) {
+        return prenotazioneRepository.findByPostazioneAndDataPrenotazione(postazione, dataPrenotazione);
+    }
+
+    public List<Prenotazione> findByUtenteAndData(Utente utente, LocalDate dataPrenotazione) {
+        return prenotazioneRepository.findByUtenteAndDataPrenotazione(utente, dataPrenotazione);
+    }
 }
