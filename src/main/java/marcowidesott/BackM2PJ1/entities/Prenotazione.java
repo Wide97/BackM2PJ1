5832de165e92679dc31124701ad84be475255d3a
Wide97 <mwidesott@gmail.com>
2024-10-25 14:26:36 +0200

    finish classes

--- src/main/java/marcowidesott/BackM2PJ1/entities/Prenotazione.java
+++ src/main/java/marcowidesott/BackM2PJ1/entities/Prenotazione.java
@@ -1,4 +1,33 @@
 package marcowidesott.BackM2PJ1.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Prenotazione {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "utente_id", nullable = false)
+    private Utente utente;
+
+    @ManyToOne
+    @JoinColumn(name = "postazione_id", nullable = false)
+    private Postazione postazione;
+
+    private LocalDate dataPrenotazione;
+
 }
--- src/main/java/marcowidesott/BackM2PJ1/entities/Utente.java
+++ src/main/java/marcowidesott/BackM2PJ1/entities/Utente.java
@@ -1,9 +1,28 @@
 package marcowidesott.BackM2PJ1.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Utente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
     private String username;
     private String nomeCompleto;
     private String email;
+
+    @OneToMany(mappedBy = "utente")
+    private List<Prenotazione> prenotazioni;
 }
